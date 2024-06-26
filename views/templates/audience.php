96e0d9c9c1f2facf7560ed599c4ea76f856d24d0
ALAN <alanrouxel@hotmail.com>
2024-06-26 09:38:10 -0400

    remplacement dun if par une ternaire dans audience.php

--- views/templates/audience.php
+++ views/templates/audience.php
@@ -14,23 +14,23 @@
                 <div class="articleLine">
                     <div class="title">
                         <a href="index.php?action=showAudience&ordercolumn=title&ordertype=<?= $column ==='title' && $type === 'ASC' ?'DESC' : 'ASC' ?>">
-                            Titres des articles <?if($column === 'title'){if($type ==='ASC'){echo "  ↓";}else{echo " ↑";}}?>
+                            Titres des articles <?=($column === 'title' && $type === 'ASC') ? "  ↓" : (($column === 'title' && $type === 'DESC') ? " ↑" : "")?>
                         </a>
                     </div>
                     <div class="count_view">
                         <a href="index.php?action=showAudience&ordercolumn=count_view&ordertype=<?=$column ==='count_view' && $type === 'ASC' ?'DESC' : 'ASC' ?>">
-                            Nombre de vues <?if($column === 'count_view'){if($type ==='ASC'){echo "  ↓";}else{echo " ↑";}}?>
+                            Nombre de vues <?=($column === 'count_view' && $type === 'ASC') ? "  ↓" : (($column === 'count_view' && $type === 'DESC') ? " ↑" : "")?>
                         
                         </a>
                     </div>
                     <div class="count_comment">
                         <a href="index.php?action=showAudience&ordercolumn=count_comment&ordertype=<?=$column ==='count_comment' && $type === 'ASC' ?'DESC' : 'ASC' ?>">
-                            Nombre de commentaires <?if($column === 'count_comment'){if($type ==='ASC'){echo "  ↓";}else{echo " ↑";}}?>
+                            Nombre de commentaires <?=($column === 'count_comment' && $type === 'ASC') ? "  ↓" : (($column === 'count_comment' && $type === 'DESC') ? " ↑" : "")?>
                         </a>
                     </div>
                     <div class="date_creation">
                         <a href="index.php?action=showAudience&ordercolumn=date_creation&ordertype=<?=$column ==='date_creation' && $type === 'ASC' ?'DESC' : 'ASC' ?>">
-                            Date de publication <?if($column === 'date_creation'){if($type ==='ASC'){echo "  ↓";}else{echo " ↑";}}?>
+                            Date de publication <?=($column === 'date_creation' && $type === 'ASC') ? "  ↓" : (($column === 'date_creation' && $type === 'DESC') ? " ↑" : "")?>
                         </a>
                     </div>
                 </div>
